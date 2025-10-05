0a6372e822393dd1cc8b14e842b67e4a1f0b0baf
Nicao01 <nicopradobueno@gmail.com>
2025-10-05 11:26:27 -0300

    feat: add initial Spring Boot application setup

---
+++ spendflux/src/main/java/com/project/spendflux/SpendfluxApplication.java
@@ -0,0 +1,13 @@
+package com.project.spendflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpendfluxApplication.class, args);
+	}
+
+}
